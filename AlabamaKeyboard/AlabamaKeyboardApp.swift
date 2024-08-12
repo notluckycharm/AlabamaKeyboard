5a26f52724f1e143835580a5bd326271f526f5eb
notluckycharm <70812022+notluckycharm@users.noreply.github.com>
2024-08-12 09:55:52 -0700

    Initial Commit

---
+++ AlabamaKeyboard/AlabamaKeyboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlabamaKeyboardApp.swift
+//  AlabamaKeyboard
+//
+//  Created by Jacob Fernandes on 8/12/24.
+//
+
+import SwiftUI
+
+@main
+struct AlabamaKeyboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
